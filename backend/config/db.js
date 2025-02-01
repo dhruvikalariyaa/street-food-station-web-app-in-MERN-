86caa2244d169b932a84b0d95eaa9cb687638faa
Dhruvi Kalariya <149812697+dhruvikalariyaa@users.noreply.github.com>
2025-02-01 22:34:05 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";    
 
 export const connectDB =async()=>{
-    await mongoose.connect('mongodb+srv://admin:12345@cluster0.iu3b2ph.mongodb.net/FOOD-DELIVERY?retryWrites=true&w=majority').then(()=>console.log("DB connected"));
+    await mongoose.connect('your mongodb url').then(()=>console.log("DB connected"));
 }
